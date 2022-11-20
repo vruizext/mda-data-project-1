b99289921919c59bf22e4b10f3b61ad8df069100
Victor Ruiz <vruizext@gmail.com>
2022-11-20 17:00:14 +0100

    Insertar composiciones

--- generador/main.py
+++ generador/main.py
@@ -4,7 +4,6 @@ import time
 
 
 def connect():
-    """Conecta con PostgresDB"""
     try:
         return psycopg2.connect(user="postgres",
                                 password="postgres",
@@ -13,6 +12,61 @@ def connect():
                                 database="iip_db")
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
+
+def insert_producto(conn, nombre, descripcion, categoria, precio):
+    try:
+        cursor = conn.cursor()
+        sql = """
+            INSERT INTO 
+            productos (nombre,descripcion,categoria,precio) 
+            VALUES (%s, %s, %s, %s)
+        """
+        cursor.execute(sql, (nombre, descripcion, categoria, precio))
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error al insertar producto ({nombre}, {descripcion}, {categoria}, {precio})", error)
+    finally:
+        if conn:
+            cursor.close()
+
+
+def insert_productos(conn, file_path):
+    with open(file_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=';')
+        for row in csv_reader:
+            insert_producto(conn, row['name'], row['short_description'], row['category'], row['price'])
+    conn.commit()
+
+
+def insert_composicion(conn, nombre, influencer_id):
+    try:
+        cursor = conn.cursor()
+        sql = """
+            INSERT INTO composiciones (nombre, influencer_id) VALUES (%s,%s)
+        """
+        cursor.execute(sql, (nombre, influencer_id))
+        conn.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error al insertar composicion ({nombre}, {influencer_id})", error)
+    finally:
+        if conn:
+            cursor.close()
+
+
+def insert_productos_comp(conn, composicion_id, producto_id):
+    try:
+        cursor = conn.cursor()
+        sql = """ 
+            INSERT INTO productos_comp (composicion_id, producto_id) VALUES (%s,%s)
+        """
+        cursor.execute(sql, (composicion_id, producto_id))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error al insertar productos composicion ({composicion_id}, {producto_id})", error)
+    finally:
+        if conn:
+            cursor.close()
 
 
 def insert_visita(conn, user_id, composicion_id, timestamp):
@@ -27,35 +81,10 @@ def insert_visita(conn, user_id, composicion_id, timestamp):
         cursor.execute(sql, (user_id, composicion_id, timestamp))
 
     except (Exception, psycopg2.Error) as error:
-        print("Error al insertar visita", error)
-    finally:
-        if conn:
-            conn.commit()
-            cursor.close()
-
-
-def insert_producto(conn, nombre, descripcion, categoria, precio):
-    try:
-        cursor = conn.cursor()
-        sql = """
-            INSERT INTO 
-            productos (nombre,descripcion,categoria,precio) 
-            VALUES (%s, %s, %s, %s)
-        """
-        cursor.execute(sql, (nombre, descripcion, categoria, precio))
-    except (Exception, psycopg2.Error) as error:
-        print("Error al insertar producto", error)
+        print(f"Error al insertar visita ({user_id}, {composicion_id}, {timestamp})", error)
     finally:
         if conn:
             cursor.close()
-
-
-def insert_productos(conn, file_path):
-    with open(file_path) as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=';')
-        for row in csv_reader:
-            insert_producto(conn, row['name'], row['short_description'], row['category'], row['price'])
-    conn.commit()
 
 
 if __name__ == '__main__':
