a46086fdb61457853c3070265c5383480aaca697
IVAN RODRIGUEZ <IRODRIGUEZ@NOR.LAN>
2022-11-23 19:47:34 +0100

    Creamos bucle

--- generador/insertar_venta.py
+++ generador/insertar_venta.py
@@ -1,5 +1,5 @@
 from main import *
-import random
+import random,time
 
 
 # Creamos la función para hacer una query aleatoria
@@ -14,41 +14,44 @@ def select_random(conn,lim):
     return cursor.fetchall()
         
 if __name__ == '__main__':
-    conn = connect()
     
-    # Marcamos el límite de los productos random
+    while True:
 
-    lim = random.randint(1,10)
+        conn = connect()
+        
+        # Asignamos un usuario aleatorio
 
-    # Seleccionamos productos de forma aleatoria
+        user_id = random.randint(1000,10000)
 
-    productos_random = select_random(conn,lim)
+        # Insertamos una visita de usuario
+
+        insert_visita(conn, user_id, 0 , datetime.now(timezone.utc))
+
+        # Marcamos el límite de los productos random
+
+        lim = random.randint(1,10)
+
+        # Seleccionamos productos de forma aleatoria
+
+        productos_random = select_random(conn,lim)
+
+        # Hace la suma de los precios de todos los productos seleccionados
+
+        total = 0
+
+        for producto in productos_random:
+            total += round(producto[4],2)
+
+        # Recogemos el id asignado a la venta en la variable venta_id
+
+        venta_id = insert_venta(conn, user_id, total, datetime.now(timezone.utc))
+        
+        # Inserta tantas lineas de venta como productos que corresponden a una venta
+
+        unidades = 1
+        
+        for producto in productos_random:
+            insert_linea_venta(conn, venta_id, producto[0],unidades,round(producto[4],2))
     
-    # Hace la suma de los precios de todos los productos seleccionados
-
-    total = 0
-
-    for producto in productos_random:
-       total += round(producto[4],2)
-    
-    # Asignamos un usuario aleatorio
-
-    user_id = random.randint(1000,10000)
-
-    # Recogemos el id asignado a la venta en la variable venta_id
-
-    venta_id = insert_venta(conn, user_id, total, datetime.now(timezone.utc))
-    
-    # Inserta tantas lineas de venta como productos que corresponden a una venta
-
-    unidades = 1
-    
-    for producto in productos_random:
-        insert_linea_venta(conn, venta_id, producto[0],unidades,round(producto[4],2))
-
-
-    #insert_venta(conn, user_id, total, datetime.now(timezone.utc))
-
-
-
-
+        print(venta_id)
+        time.sleep(3)
