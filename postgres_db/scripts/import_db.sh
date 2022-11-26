15d43949b484597dbbecbe2ed38c3f80cb63bb38
Victor Ruiz <vruizext@gmail.com>
2022-11-26 16:10:37 +0100

    Arreglar script de inicializacion

--- postgres_db/scripts/import_db.sh
+++ postgres_db/scripts/import_db.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-psql -U postgres -c "DROP DATABASE IF EXISTS $POSTGRES_DB;"
-createdb -U $POSTGRES_USER $POSTGRES_DB
-psql -U postgres -d $POSTGRES_DB -f iip_db.sql
+dropdb -U postgres -f $POSTGRES_DB && createdb -U $POSTGRES_USER $POSTGRES_DB && psql -U postgres -d $POSTGRES_DB -f iip_db.sql
